959cf9a5ac1dfa5b45f4b92b0f4d3dbaedc22bc2
mikedr <derosamiguel@gmail.com>
2022-12-17 17:10:29 -0300

    Sobrecarga de métodos

--- scalaAppExample/src/main/scala/Person.scala
+++ scalaAppExample/src/main/scala/Person.scala
@@ -1,3 +1,4 @@
 class Person(val name: String, val age: Int) {
-  def greet(): String = "Hi, my name is"+this.name
+  def greet(): String = "Hi, my name is "+this.name
+  def greet(yourName: String): String = s"Hi $yourName, my name is $name"
 }
