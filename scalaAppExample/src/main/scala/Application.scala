28f1dd62b66ca8a7662ae4808bf544776082a316
mikedr <derosamiguel@gmail.com>
2022-12-26 13:49:33 -0300

    Ejercicio

--- scalaAppExample/src/main/scala/Application.scala
+++ scalaAppExample/src/main/scala/Application.scala
@@ -1,3 +1,13 @@
 object Application extends App {
+  def aWriter = new Writer("Mario","Puzo",1920)
+  println("Nombre del escritor: "+aWriter.fullName())
 
+  def aNovel = new Novel("The Godfather", 1969, aWriter)
+  println("Tiempo de autor: "+aNovel.authorAge()+" años")
+
+  def otherWriter = new Writer("Jorge","Borges", 1899)
+  println(aNovel.name+" está escrita por: "+otherWriter.fullName()+"?: "+aNovel.isWrittenBy(otherWriter))
+
+  def unaCopia = aNovel.copy(aNovel,1980)
+  println("La copia se publicó en: "+unaCopia.yearRelease)
 }
